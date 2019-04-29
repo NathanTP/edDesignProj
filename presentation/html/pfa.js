b856da66307575056c643af5b919595426cf7a21
Nathan Pemberton <nathanp@berkeley.edu>
2019-04-29 11:59:44 -0700

    auto-run cells works

--- presentation/html/pfa.js
+++ presentation/html/pfa.js
@@ -10,6 +10,7 @@ function GetElementInsideContainer(containerID, childID) {
     return elm;
 }
 
+// Tabbed figures
 function openFig(evt, contID, figID) {
   // Declare all variables
   var i, tabcontent, tablinks, cont;
@@ -38,5 +39,36 @@ function openFig(evt, contID, figID) {
   document.getElementById(figID).style.display = "block";
   evt.currentTarget.className += " active";
 }
+
+// Collapsible
+function clickCollapsible(id) {
+	var elm = document.getElementById(id);
+	elm.classList.toggle("active");
+	var content = elm.nextElementSibling;
+	if (content.style.display === "block") {
+		content.style.display = "none";
+	} else {
+		content.style.display = "block";
+	}
+}
+
+// Once thebelab is ready, auto-run all the cells in a "thebeAuto" div
+thebelab.on("status", function(evt, data) {
+		if (data.status == "ready") {
+      // Find my special "auto-run" divs
+			var autos = document.getElementsByClassName("thebeAuto");
+			for (i = 0; i < autos.length; i++) {
+				// Get all the "thebelab-cell" elements
+				var cells = autos[i].childNodes;
+				for (j = 0; j < cells.length; j++) {
+					if (cells[j].className == "thebelab-cell") {
+            console.log("Auto-running thebeCell. Ignore the following error")
+            console.log(cells[j])
+            cells[j].children[1].click()
+					}
+				}
+			}
+		}
+})
 
 
